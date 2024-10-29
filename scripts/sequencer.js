f743f62a183eeb4e5365188637c2b36e260990e0
Matt Thompson <matthew.thompson6@icloud.com>
2024-10-29 15:18:45 +0200

    Add sequencer page

--- scripts/sequencer.js
+++ scripts/sequencer.js
@@ -9,25 +9,33 @@ channelButton.forEach(function (channelButton) {
   });
 });
 
-let intervalRate = 500;
+let intervalRate = 280;
 let intervalId;
 let currentStep = 0;
 let stepIndicators = document.querySelectorAll(".step-indicator");
 let kick = document.getElementById("kick");
 let hat = document.getElementById("hat");
 let snare = document.getElementById("snare");
-let fx = document.getElementById("fx");
+let open = document.getElementById("open");
+let chordI = document.getElementById("chordI");
+let chordV = document.getElementById("chordV");
+let chordVI = document.getElementById("chordVI");
+let chordIV = document.getElementById("chordIV");
 let channel1 = document.querySelectorAll(".ch1");
 let channel2 = document.querySelectorAll(".ch2");
 let channel3 = document.querySelectorAll(".ch3");
 let channel4 = document.querySelectorAll(".ch4");
+
+let chordArray = [chordI, chordV, chordVI, chordIV];
+
+let generateChords = false;
 
 const totalIndicators = stepIndicators.length;
 const angleIncrement = 360 / totalIndicators;
 
 stepIndicators.forEach((indicator, index) => {
   const angle = angleIncrement * index;
-  indicator.style.transform = `rotate(${angle}deg) translate(0, -60px)`;
+  indicator.style.transform = `rotate(${angle}deg) translate(0, -30px)`;
 });
 
 function updateInterval() {
@@ -59,7 +67,11 @@ function loopSteps() {
     stepIndicators[currentStep].classList.contains("active") &&
     channel4[currentStep].classList.contains("active")
   ) {
-    fx.play();
+    open.play();
+  }
+
+  if (generateChords === true && (currentStep == 0 || currentStep == 7)) {
+    randomChord();
   }
 
   // Remove 'active' class from all steps
@@ -75,17 +87,50 @@ function loopSteps() {
 function increaseTempo() {
   if (intervalRate > 250) {
     intervalRate /= 1.13;
-    console.log("New interval rate:", intervalRate);
     updateInterval();
   }
 }
 
 function tempoMinus() {
   intervalRate *= 1.13;
-  console.log("New interval rate:", intervalRate);
   updateInterval();
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   updateInterval();
+});
+
+function playChordI() {
+  chordI.play();
+}
+function playChordV() {
+  chordV.play();
+}
+function playChordVI() {
+  chordVI.play();
+}
+function playChordIV() {
+  chordIV.play();
+}
+
+let previousNumber = null;
+
+function randomChord() {
+  let randomNumber;
+  do {
+    randomNumber = Math.floor(Math.random() * 4);
+  } while (randomNumber === previousNumber);
+
+  previousNumber = randomNumber;
+
+  let chordToPlay = chordArray[randomNumber];
+  chordToPlay.play();
+}
+
+// Select the checkbox element
+let switchInput = document.getElementById("chordGenSwitch");
+
+// Add an event listener to toggle generateChords
+switchInput.addEventListener("change", function () {
+  generateChords = this.checked;
 });
